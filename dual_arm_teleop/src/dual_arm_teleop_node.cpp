eee54ba77d0c90bd9d3b125d9e11f08e563e4330
Daniel Chen <danieljnchen@gmail.com>
2018-08-13 14:16:10 -0400

    Integrate republisher into main node

--- dual_arm_teleop/src/dual_arm_teleop_node.cpp
+++ dual_arm_teleop/src/dual_arm_teleop_node.cpp
@@ -10,15 +10,17 @@
 #include <moveit_msgs/DisplayRobotState.h>
 
 // Vive
-#include "vive_msgs/ViveSystem.h"
+#include <vive_msgs/ViveSystem.h>
 
 // Victor
-#include "victor_hardware_interface/MotionCommand.h"
-#include "victor_hardware_interface/Robotiq3FingerCommand.h"
+#include <victor_hardware_interface/MotionCommand.h>
+#include <victor_hardware_interface/MotionStatus.h>
+#include <victor_hardware_interface/Robotiq3FingerCommand.h>
+#include <victor_hardware_interface/Robotiq3FingerStatus.h>
 
 // TF
 #include <tf/transform_broadcaster.h>
-#include "tf_conversions/tf_eigen.h"
+#include <tf_conversions/tf_eigen.h>
 
 // rviz
 #include <rviz_visual_tools/rviz_visual_tools.h>
@@ -52,7 +54,6 @@ struct victor_arm
 {
   victor_arm() : activated(false), trackpad_pressed(false), enabled(false), initialized(false), ee_start_translation(Eigen::Vector3d::Identity()) {}
 
-  bool publishing = true;
   bool activated; // true if arm is currently being teleoperated
   bool trackpad_pressed;
   bool enabled; // true if arm has been paired with a controller
@@ -60,21 +61,25 @@ struct victor_arm
 
   int assigned_controller_index;
   int assigned_controller_id;
-  Eigen::Vector3d ee_start_translation;
-  Eigen::Vector3d controller_start_translation;
-  Eigen::Quaterniond controller_start_rotation;
-  Eigen::Affine3d last_valid_pose;
-
-  visualization_msgs::Marker err_msg;
-
-  // State publishers
-  ros::Publisher pub_arm;
-  ros::Publisher pub_gripper;
 
   // Kinematics
   std::string joint_model_group_name;
   robot_state::JointModelGroup* joint_model_group;
-  std::vector<std::string> joint_names;
+
+  Eigen::Vector3d ee_start_translation;
+  Eigen::Vector3d controller_start_translation;
+  Eigen::Quaterniond controller_start_rotation;
+  Eigen::Affine3d last_valid_pose;
+  Eigen::Affine3d last_valid_synced_pose;
+
+  std::vector<double> joint_position_measured;
+
+  // Topic publishers/subscribers
+  ros::Publisher pub_arm;
+  ros::Publisher pub_gripper;
+  ros::Subscriber sub_arm_status;
+
+  visualization_msgs::Marker err_msg;
 };
 
 class DualArmTeleop
@@ -86,6 +91,7 @@ public:
 
     // Initialize victor kinematic model
     robot_model_loader::RobotModelLoader robot_model_load("robot_description");
+
     kinematic_model = robot_model_load.getModel();
     kinematic_state = std::make_shared<robot_state::RobotState>(kinematic_model);
 
@@ -100,27 +106,28 @@ public:
     //rvt = rviz_visual_tools::RvizVisualTools("victor_root", "err_msg", n);
     //pub_err_msg = n.advertise<visualization_msgs::Marker>("err_msg", 10);
 
+    pub_rviz = n.advertise<moveit_msgs::DisplayRobotState>("display_robot_state", 1);
+
     for (int arm = 0; arm < 2; ++arm) {
-      victor_arms[arm].joint_model_group = kinematic_model->getJointModelGroup(
-              victor_arms[arm].joint_model_group_name);
-      victor_arms[arm].joint_names = victor_arms[arm].joint_model_group->getVariableNames();
+      victor_arms[arm].joint_model_group = kinematic_model->getJointModelGroup(victor_arms[arm].joint_model_group_name);
 
-      victor_arms[arm].last_valid_pose = kinematic_state->getGlobalLinkTransform(
-              "victor_" + victor_arms[arm].joint_model_group_name + "_link_7");
-
+      victor_arms[arm].last_valid_pose = kinematic_state->getGlobalLinkTransform("victor_" + victor_arms[arm].joint_model_group->getName() + "_link_7");
+      victor_arms[arm].last_valid_synced_pose = kinematic_state->getGlobalLinkTransform("victor_" + victor_arms[arm].joint_model_group->getName() + "_link_7");
       victor_arms[arm].ee_start_translation = victor_arms[arm].last_valid_pose.translation();
 
-      victor_arms[arm].pub_arm = n.advertise<victor_hardware_interface::MotionCommand>(
-              victor_arms[arm].joint_model_group_name + "/motion_command", 10);
-      victor_arms[arm].pub_gripper = n.advertise<victor_hardware_interface::Robotiq3FingerCommand>(
-              victor_arms[arm].joint_model_group_name + "/gripper_command", 10);
-      /*victor_arms[arm].pub_arm = n.advertise<victor_hardware_interface::MotionCommand>(
-              "unchecked_victor/" + victor_arms[arm].joint_model_group_name + "/motion_command", 10);
-      victor_arms[arm].pub_gripper = n.advertise<victor_hardware_interface::Robotiq3FingerCommand>(
-              "unchecked_victor/" + victor_arms[arm].joint_model_group_name + "/gripper_command", 10);*/
+      victor_arms[arm].pub_arm = n.advertise<victor_hardware_interface::MotionCommand>(victor_arms[arm].joint_model_group->getName() + "/motion_command", 10);
+      victor_arms[arm].pub_gripper = n.advertise<victor_hardware_interface::Robotiq3FingerCommand>(victor_arms[arm].joint_model_group->getName() + "/gripper_command", 10);
 
-      pub_rviz = n.advertise<moveit_msgs::DisplayRobotState>("display_robot_state", 1);
+      if (arm == 0)
+      {
+        victor_arms[arm].sub_arm_status = n.subscribe(victor_arms[arm].joint_model_group->getName() + "/motion_status", 10, &DualArmTeleop::updateArmStatusLeft, this);
+      }
+      else
+      {
+        victor_arms[arm].sub_arm_status = n.subscribe(victor_arms[arm].joint_model_group->getName() + "/motion_status", 10, &DualArmTeleop::updateArmStatusRight, this);
+      }
 
+      // Visualization messages
       victor_arms[arm].err_msg = visualization_msgs::Marker();
       victor_arms[arm].err_msg.header.frame_id = "victor_root";
       victor_arms[arm].err_msg.ns = "my_namespace";
@@ -185,14 +192,7 @@ public:
 
     for (int arm = 0; arm < 2; ++arm)
     {
-      /*if (victor_arms[arm].publishing) {
-        std::cout << "Published msg for arm " + std::to_string(arm) << std::endl;
-        pub_err_msg.publish(victor_arms[arm].err_msg);
-        //victor_arms[arm].publishing = false;
-      }*/
       Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
-      //rvt.publishText()
-      //rvt.publishText(text_pose, "hello");
 
       // Skip this arm if no controller is assigned to it
       if (!victor_arms[arm].enabled)
@@ -222,14 +222,14 @@ public:
       {
         //victor_arms[arm].ee_start_pose = victor_arms[arm].last_valid_pose;
         /*Eigen::Transform<double, 3, 2, 0>::LinearMatrixType rot = kinematic_state->getGlobalLinkTransform(
-          "victor_" + victor_arms[arm].joint_model_group_name + "_link_7").linear();
+          "victor_" + victor_arms[arm].joint_model_group->getName() + "_link_7").linear();
         victor_arms[arm].ee_start_rotation = viveToVictorRotation(Eigen::Quaterniond(rot[0], rot[1], rot[2], rot[3]));*/
         /*victor_arms[arm].ee_start_pose = kinematic_state->getGlobalLinkTransform(
-          "victor_" + victor_arms[arm].joint_model_group_name + "_link_7");*/
+          "victor_" + victor_arms[arm].joint_model_group->getName() + "_link_7");*/
 
         // Controller frame
-        victor_arms[arm].controller_start_translation = msgToEigenPoint(msg_controller.posestamped.pose.position);
-        //victor_arms[arm].controller_start_rotation = msgToEigenQuaternion(msg_controller.posestamped.pose.orientation);
+        victor_arms[arm].controller_start_translation = pointMsgToEigen(msg_controller.posestamped.pose.position);
+        //victor_arms[arm].controller_start_rotation = quatMsgToEigen(msg_controller.posestamped.pose.orientation);
         //victor_arms[arm].controller_start_rotation = Eigen::Quaterniond::Identity();
 
         victor_arms[arm].initialized = true;
@@ -237,31 +237,28 @@ public:
 
       // Reset orientation
       if (msg_controller.joystick.buttons[1] == 2) {
-        victor_arms[arm].controller_start_rotation = msgToEigenQuaternion(msg_controller.posestamped.pose.orientation);
+        victor_arms[arm].controller_start_rotation = quatMsgToEigen(msg_controller.posestamped.pose.orientation);
       }
 
-
-      //Calculate the relative pose
+      // Calculate the relative pose
       Eigen::Affine3d relative_pose = Eigen::Affine3d::Identity();
 
-      //Base
+      // Base
       relative_pose.translate(victor_arms[arm].ee_start_translation);
 
-      //Translation
+      // Translation
       Eigen::Vector3d translation(0, 0, 0);
-      translation += viveToVictorTranslation(msgToEigenPoint(msg_controller.posestamped.pose.position));
+      translation += viveToVictorTranslation(pointMsgToEigen(msg_controller.posestamped.pose.position));
       translation -= viveToVictorTranslation(victor_arms[arm].controller_start_translation);
-      translation *= (msg_controller.joystick.axes[1] + 1.5) * .5;
-      if (translation.norm() < .1)
-      {
-        relative_pose.translate(translation);
-      }
+      translation *= (msg_controller.joystick.axes[1] + 1.5) * .5; // motion scaling
+
+      relative_pose.translate(translation);
 
       // Rotation
-      relative_pose.rotate(viveToVictorRotation(msgToEigenQuaternion(msg_controller.posestamped.pose.orientation)));
+      relative_pose.rotate(viveToVictorRotation(quatMsgToEigen(msg_controller.posestamped.pose.orientation)));
       relative_pose.rotate(viveToVictorRotation(victor_arms[arm].controller_start_rotation).inverse());
 
-      // Compute IK solution
+      // Generate IK solutions
       const kinematics::KinematicsBaseConstPtr& solver = victor_arms[arm].joint_model_group->getSolverInstance();
       assert(solver.get());
 
@@ -283,10 +280,7 @@ public:
       pose.orientation.w = q.w();
       target_poses.push_back(pose);
 
-      std::vector<double> seed;
-      kinematic_state->copyJointGroupPositions(victor_arms[arm].joint_model_group, seed);
-      //std::vector<double> seed(victor_arms[arm].joint_model_group->getVariableCount());
-
+      std::vector<double> seed = victor_arms[arm].joint_position_measured;
       std::vector<std::vector<double>> solutions;
       kinematics::KinematicsResult result;
       kinematics::KinematicsQueryOptions options;
@@ -294,44 +288,39 @@ public:
 
       solver->getPositionIK(target_poses, seed, solutions, result, options);
 
+      // Pick the solution that matches closest to the measured joint state
       if (!solutions.empty()) {
+        SeedDistanceFunctor functor(seed);
+        std::priority_queue<std::vector<double>, std::vector<std::vector<double>>, SeedDistanceFunctor> slnQueue(solutions.begin(), solutions.end(), functor);
+        kinematic_state->setJointGroupPositions(victor_arms[arm].joint_model_group, slnQueue.top());
+
         victor_arms[arm].last_valid_pose = relative_pose;
         victor_arms[arm].ee_start_translation = relative_pose.translation();
 
-        SeedDistanceFunctor functor(seed);
-        std::priority_queue<std::vector<double>, std::vector<std::vector<double>>, SeedDistanceFunctor>
-                                                                                   slnQueue(solutions.begin(), solutions.end(), functor);
-        kinematic_state->setJointGroupPositions(victor_arms[arm].joint_model_group, slnQueue.top());
-
-        //ROS_INFO("Found IK solution");
         victor_arms[arm].err_msg.text = "";
       }
       else
       {
         victor_arms[arm].err_msg.text = "Did not find IK solution";
-        //ROS_INFO("Did not find IK solution");
       }
 
-      std::cerr << "Got " << solutions.size() << " solutions ";
-      if (arm == 1) {
-        std::cerr << std::endl;
-      }
+      std::cerr << "Got " << solutions.size() << " solutions for " << victor_arms[arm].joint_model_group->getName() << std::endl;
 
       // Arm control
-      victor_hardware_interface::MotionCommand msg_out_motion;
+      victor_hardware_interface::MotionCommand msg_out_arm;
 
       std::vector<double> joint_values;
       kinematic_state->copyJointGroupPositions(victor_arms[arm].joint_model_group, joint_values);
 
-      msg_out_motion.joint_position.joint_1 = joint_values[0];
-      msg_out_motion.joint_position.joint_2 = joint_values[1];
-      msg_out_motion.joint_position.joint_3 = joint_values[2];
-      msg_out_motion.joint_position.joint_4 = joint_values[3];
-      msg_out_motion.joint_position.joint_5 = joint_values[4];
-      msg_out_motion.joint_position.joint_6 = joint_values[5];
-      msg_out_motion.joint_position.joint_7 = joint_values[6];
+      msg_out_arm.joint_position.joint_1 = joint_values[0];
+      msg_out_arm.joint_position.joint_2 = joint_values[1];
+      msg_out_arm.joint_position.joint_3 = joint_values[2];
+      msg_out_arm.joint_position.joint_4 = joint_values[3];
+      msg_out_arm.joint_position.joint_5 = joint_values[4];
+      msg_out_arm.joint_position.joint_6 = joint_values[5];
+      msg_out_arm.joint_position.joint_7 = joint_values[6];
 
-      victor_arms[arm].controller_start_translation = msgToEigenPoint(msg_controller.posestamped.pose.position);
+      victor_arms[arm].controller_start_translation = pointMsgToEigen(msg_controller.posestamped.pose.position);
 
       victor_arms[arm].err_msg.id = arm;
       victor_arms[arm].err_msg.pose.position.x = relative_pose.translation()[0];
@@ -369,30 +358,70 @@ public:
       msg_out_gripper.finger_c_command = finger_c;
 
       // Publish state messages
-      victor_arms[arm].pub_arm.publish(msg_out_motion);
+      if (armWithinDelta(jvqToVector(msg_out_arm.joint_position), arm)) {
+        victor_arms[arm].pub_arm.publish(msg_out_arm);
+
+        victor_arms[arm].last_valid_synced_pose = relative_pose;
+      }
+
       victor_arms[arm].pub_gripper.publish(msg_out_gripper);
 
       // Display rviz poses
       tf::Transform transform1;
       tf::poseEigenToTF(relative_pose, transform1);
-      tf_broadcaster.sendTransform(tf::StampedTransform(transform1, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group_name + "/relative_pose"));
+      tf_broadcaster.sendTransform(tf::StampedTransform(transform1, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group->getName() + "/relative_pose"));
 
       tf::Transform transform2;
       tf::poseEigenToTF(translationAndRotationToAffine(victor_arms[arm].controller_start_translation, victor_arms[arm].controller_start_rotation), transform2);
-      tf_broadcaster.sendTransform(tf::StampedTransform(transform2, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group_name + "/reset_pose"));
+      tf_broadcaster.sendTransform(tf::StampedTransform(transform2, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group->getName() + "/reset_pose"));
 
       tf::Transform transform3;
-      tf::poseEigenToTF(msgToEigenPose(msg_controller.posestamped.pose), transform3);
-      tf_broadcaster.sendTransform(tf::StampedTransform(transform3, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group_name + "/global_pose"));
+      tf::poseEigenToTF(poseMsgToEigen(msg_controller.posestamped.pose), transform3);
+      tf_broadcaster.sendTransform(tf::StampedTransform(transform3, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group->getName() + "/global_pose"));
 
       tf::Transform transform4;
       tf::poseEigenToTF(victor_arms[arm].last_valid_pose, transform4);
-      tf_broadcaster.sendTransform(tf::StampedTransform(transform4, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group_name + "/last_valid_pose"));
+      tf_broadcaster.sendTransform(tf::StampedTransform(transform4, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group->getName() + "/last_valid_pose"));
+
+      tf::Transform transform5;
+      tf::poseEigenToTF(victor_arms[arm].last_valid_synced_pose, transform5);
+      tf_broadcaster.sendTransform(tf::StampedTransform(transform5, ros::Time::now(), "victor_root", victor_arms[arm].joint_model_group->getName() + "/last_valid_synced_pose"));
     }
 
     moveit_msgs::DisplayRobotState display_robot_state;
     robot_state::robotStateToRobotStateMsg(*kinematic_state, display_robot_state.state);
     pub_rviz.publish(display_robot_state);
+  }
+
+  void updateArmStatus(victor_hardware_interface::MotionStatus msg, int arm) {
+    victor_arms[arm].joint_position_measured = jvqToVector(msg.measured_joint_position);
+  }
+
+  void updateArmStatusLeft(victor_hardware_interface::MotionStatus msg)
+  {
+    updateArmStatus(msg, 0);
+  }
+
+  void updateArmStatusRight(victor_hardware_interface::MotionStatus msg)
+  {
+    updateArmStatus(msg, 1);
+  }
+
+  bool armWithinDelta(std::vector<double> joint_position_commanded, int arm)
+  {
+    assert(joint_position_commanded.size() == victor_arms[arm].joint_position_measured.size());
+
+    double distance = 0;
+
+    for (int i = 0; i < joint_position_commanded.size(); ++i) {
+      distance += pow(joint_position_commanded[i] - victor_arms[arm].joint_position_measured[i], 2);
+    }
+
+    distance = sqrt(distance);
+
+    std::cout << "Joint space error for " << victor_arms[arm].joint_model_group->getName() << ": " << distance << std::endl;
+
+    return distance < 1.5;
   }
 
   Eigen::Affine3d translationAndRotationToAffine(Eigen::Vector3d translation, Eigen::Quaterniond rotation)
@@ -423,7 +452,7 @@ public:
   }
 
 
-  Eigen::Vector3d msgToEigenPoint(geometry_msgs::Point point)
+  Eigen::Vector3d pointMsgToEigen(geometry_msgs::Point point)
   {
     return Eigen::Vector3d(
       point.x,
@@ -432,7 +461,7 @@ public:
     );
   }
 
-  Eigen::Quaterniond msgToEigenQuaternion(geometry_msgs::Quaternion quaternion)
+  Eigen::Quaterniond quatMsgToEigen(geometry_msgs::Quaternion quaternion)
   {
     return Eigen::Quaterniond(
       quaternion.x,
@@ -442,15 +471,17 @@ public:
     );
   }
 
-  Eigen::Affine3d msgToEigenPose(geometry_msgs::Pose pose)
+  Eigen::Affine3d poseMsgToEigen(geometry_msgs::Pose pose)
   {
-    Eigen::Affine3d affine = Eigen::Affine3d::Identity();
-
-    affine.translate(msgToEigenPoint(pose.position));
-    affine.rotate(msgToEigenQuaternion(pose.orientation));
-
-    return affine;
+    return translationAndRotationToAffine(pointMsgToEigen(pose.position), quatMsgToEigen(pose.orientation));
   }
+
+  std::vector<double> jvqToVector(victor_hardware_interface::JointValueQuantity jvq)
+  {
+    std::vector<double> v{jvq.joint_1, jvq.joint_2, jvq.joint_3, jvq.joint_4,
+                          jvq.joint_5, jvq.joint_6, jvq.joint_7};
+    return v;
+  };
 
 private:
   ros::NodeHandle n;
@@ -469,7 +500,7 @@ private:
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "dual_arm_teleop_node");
-  
+
   DualArmTeleop dual_arm_teleop;
 
   ros::spin();
