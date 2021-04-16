1bdc917320fe5dddf066aefd510419e9ab49d481
Brad Saund <brad.saund@gmail.com>
2021-04-16 14:07:12 -0400

    autoformat and remove unused files

--- dual_arm_teleop/include/dual_arm_teleop/unity_teleop.hpp
+++ dual_arm_teleop/include/dual_arm_teleop/unity_teleop.hpp
@@ -3,8 +3,8 @@
 #include "dual_arm_teleop/SetEnabled.h"
 
 // MoveIt!
-#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/robot_state/conversions.h>
 #include <moveit/robot_state/robot_state.h>
 #include <moveit_msgs/DisplayRobotState.h>
@@ -18,57 +18,42 @@
 // TF
 #include <tf/transform_broadcaster.h>
 
-#include "robot_arm.h"
+#include "unity_arm.h"
 #define LEFT_IND 0
 #define RIGHT_IND 1
 
+class DualArmTeleop {
+ private:
+  ros::NodeHandle n;
+  ros::Subscriber sub_right;
+  ros::Subscriber sub_joy_right;
+  ros::Subscriber sub_left;
+  ros::Subscriber sub_joy_left;
 
-class DualArmTeleop
-{
-private:
-    ros::NodeHandle n;
-    ros::Subscriber sub_right;
-    ros::Subscriber sub_joy_right;
-    ros::Subscriber sub_left;
-    ros::Subscriber sub_joy_left;
-    
-    ros::Publisher pub_joint_state;
-    ros::ServiceServer srv_enable;
+  ros::Publisher pub_joint_state;
+  ros::ServiceServer srv_enable;
 
-    robot_model::RobotModelPtr kinematic_model;
-    robot_state::RobotStatePtr kinematic_state;
+  robot_model::RobotModelPtr kinematic_model;
+  robot_state::RobotStatePtr kinematic_state;
 
-    bool teleop_enabled=true;
+  bool teleop_enabled = true;
 
-    RobotArm* victor_arms[2];
+  RobotArm *victor_arms[2];
 
-public:
-    DualArmTeleop();
+ public:
+  DualArmTeleop();
 
-    void callbackArm(geometry_msgs::PoseStamped target_pose, int arm_ind);
+  void callbackArm(geometry_msgs::PoseStamped target_pose, int arm_ind);
 
-    void callbackRight(geometry_msgs::PoseStamped target_pose)
-    {
-        callbackArm(target_pose, RIGHT_IND);
-    }
+  void callbackRight(geometry_msgs::PoseStamped target_pose) { callbackArm(target_pose, RIGHT_IND); }
 
-    void callbackLeft(geometry_msgs::PoseStamped target_pose)
-    {
-        callbackArm(target_pose, LEFT_IND);
-    }
+  void callbackLeft(geometry_msgs::PoseStamped target_pose) { callbackArm(target_pose, LEFT_IND); }
 
-    void callbackRightJoy(sensor_msgs::Joy joy)
-    {
-        callbackJoy(joy, RIGHT_IND);
-    }
+  void callbackRightJoy(sensor_msgs::Joy joy) { callbackJoy(joy, RIGHT_IND); }
 
-    void callbackLeftJoy(sensor_msgs::Joy joy)
-    {
-        callbackJoy(joy, LEFT_IND);
-    }
+  void callbackLeftJoy(sensor_msgs::Joy joy) { callbackJoy(joy, LEFT_IND); }
 
-    void callbackJoy(sensor_msgs::Joy joy, int arm_ind);
+  void callbackJoy(sensor_msgs::Joy joy, int arm_ind);
 
-    bool setEnabledCallback(dual_arm_teleop::SetEnabled::Request &req,
-                            dual_arm_teleop::SetEnabled::Request &res);
+  bool setEnabledCallback(dual_arm_teleop::SetEnabled::Request &req, dual_arm_teleop::SetEnabled::Request &res);
 };
--- dual_arm_teleop/src/unity_arm.cpp
+++ dual_arm_teleop/src/unity_arm.cpp
@@ -1,4 +1,5 @@
 #include "unity_arm.h"
+
 #include "std_msgs/String.h"
 
 #define DELTA 0.7
@@ -8,161 +9,149 @@
 /// \param controller_hand Hand role (0 = invalid, 1 = left, 2 = right)
 /// \param kinematic_model Reference to global kinematic model
 /// \param n Reference to global node handle
-RobotArm::RobotArm(std::string joint_model_group_name, int controller_hand, robot_model::RobotModelPtr kinematic_model, robot_state::RobotStatePtr kinematic_state, ros::NodeHandle n)
-{
-    const std::string& name = joint_model_group_name;
-    this->kinematic_model = kinematic_model;
-    this->kinematic_state = kinematic_state;
+RobotArm::RobotArm(std::string joint_model_group_name, int controller_hand, robot_model::RobotModelPtr kinematic_model,
+                   robot_state::RobotStatePtr kinematic_state, ros::NodeHandle n) {
+  const std::string& name = joint_model_group_name;
+  this->kinematic_model = kinematic_model;
+  this->kinematic_state = kinematic_state;
 
-    joint_model_group = kinematic_model->getJointModelGroup(joint_model_group_name);
+  joint_model_group = kinematic_model->getJointModelGroup(joint_model_group_name);
 
-    kinematic_state->setToDefaultValues();
+  kinematic_state->setToDefaultValues();
 
-    joint_position_measured.resize(7);
-    pub_arm = n.advertise<victor_hardware_interface_msgs::MotionCommand>(name + "/motion_command", 10);
-    pub_gripper = n.advertise<victor_hardware_interface_msgs::Robotiq3FingerCommand>(
-        name + "/gripper_command", 10);
-    sub_arm_status = n.subscribe(name + "/motion_status", 10,
-                                 &RobotArm::callbackArmStatusUpdate, this);
-    pub_target = n.advertise<sensor_msgs::JointState>(name + "/target", 10);
-    pub_is_valid = n.advertise<std_msgs::String>(name + "/is_ik_valid", 10);
+  joint_position_measured.resize(7);
+  pub_arm = n.advertise<victor_hardware_interface_msgs::MotionCommand>(name + "/motion_command", 10);
+  pub_gripper = n.advertise<victor_hardware_interface_msgs::Robotiq3FingerCommand>(name + "/gripper_command", 10);
+  sub_arm_status = n.subscribe(name + "/motion_status", 10, &RobotArm::callbackArmStatusUpdate, this);
+  pub_target = n.advertise<sensor_msgs::JointState>(name + "/target", 10);
+  pub_is_valid = n.advertise<std_msgs::String>(name + "/is_ik_valid", 10);
 }
 
-void RobotArm::handleGripperCommand(double command_position)
-{
-    if(command_position != prev_gripper_command)
-    {
-        publishGripperCommand(command_position);
-        prev_gripper_command = command_position;
-    }
+void RobotArm::handleGripperCommand(double command_position) {
+  if (command_position != prev_gripper_command) {
+    publishGripperCommand(command_position);
+    prev_gripper_command = command_position;
+  }
 }
 
-std::vector<double> RobotArm::IK(geometry_msgs::PoseStamped ee_target_pose)
-{
-    Eigen::Isometry3d target;
-    tf::poseMsgToEigen(ee_target_pose.pose, target);
-    return IK(target);
+std::vector<double> RobotArm::IK(geometry_msgs::PoseStamped ee_target_pose) {
+  Eigen::Isometry3d target;
+  tf::poseMsgToEigen(ee_target_pose.pose, target);
+  return IK(target);
 }
 
-std::vector<double> RobotArm::IK(Eigen::Isometry3d ee_target_pose)
-{
-    // Generate IK solutions
-    const kinematics::KinematicsBaseConstPtr& solver = joint_model_group->getSolverInstance();
-    assert(solver.get());
+std::vector<double> RobotArm::IK(Eigen::Isometry3d ee_target_pose) {
+  // Generate IK solutions
+  const kinematics::KinematicsBaseConstPtr& solver = joint_model_group->getSolverInstance();
+  assert(solver.get());
 
-    Eigen::Isometry3d solverTrobot = Eigen::Isometry3d::Identity();
-    kinematic_state->setToIKSolverFrame(solverTrobot, solver);
+  Eigen::Isometry3d solverTrobot = Eigen::Isometry3d::Identity();
+  kinematic_state->setToIKSolverFrame(solverTrobot, solver);
 
-    // Convert to solver frame
-    Eigen::Isometry3d pt_solver = solverTrobot * ee_target_pose;
+  // Convert to solver frame
+  Eigen::Isometry3d pt_solver = solverTrobot * ee_target_pose;
 
-    std::vector<geometry_msgs::Pose> target_poses;
-    geometry_msgs::Pose pose;
-    tf::poseEigenToMsg(pt_solver, pose);
-    target_poses.push_back(pose);
+  std::vector<geometry_msgs::Pose> target_poses;
+  geometry_msgs::Pose pose;
+  tf::poseEigenToMsg(pt_solver, pose);
+  target_poses.push_back(pose);
 
-    std::vector<double> seed = joint_position_measured;
-    std::vector<std::vector<double>> solutions;
-    kinematics::KinematicsResult result;
-    kinematics::KinematicsQueryOptions options;
-    options.discretization_method = kinematics::DiscretizationMethod::ALL_DISCRETIZED;
+  std::vector<double> seed = joint_position_measured;
+  std::vector<std::vector<double>> solutions;
+  kinematics::KinematicsResult result;
+  kinematics::KinematicsQueryOptions options;
+  options.discretization_method = kinematics::DiscretizationMethod::ALL_DISCRETIZED;
 
-    solver->getPositionIK(target_poses, seed, solutions, result, options);
+  solver->getPositionIK(target_poses, seed, solutions, result, options);
 
-    // Pick the solution that matches closest to the measured joint state
-    if (solutions.empty()) {
-        std_msgs::String msg;
-        msg.data = "invalid";
-        pub_is_valid.publish(msg);
-    }
-    else
-    {
-        SeedDistanceFunctor functor(seed);
-        std::priority_queue<std::vector<double>, std::vector<std::vector<double>>, SeedDistanceFunctor> slnQueue(solutions.begin(), solutions.end(), functor);
-        kinematic_state->setJointGroupPositions(joint_model_group, slnQueue.top());
-    }
-
-    std::cerr << "Got " << solutions.size() << " solutions for " << joint_model_group->getName() << std::endl;
-
-    // Arm control
-
-    std::vector<double> joint_values;
-    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-    return joint_values;
-}
-
-void RobotArm::publishArmCommand(std::vector<double> joint_positions)
-{
-    victor_hardware_interface_msgs::MotionCommand msg_out_arm;
-    msg_out_arm.control_mode.mode = 2;
-
-    msg_out_arm.joint_position = victor_utils::vectorToJvq(joint_positions);
-    // Publish state messages
+  // Pick the solution that matches closest to the measured joint state
+  if (solutions.empty()) {
     std_msgs::String msg;
     msg.data = "invalid";
-
-    if (armWithinDelta(victor_utils::jvqToVector(msg_out_arm.joint_position), DELTA)) {
-        pub_arm.publish(msg_out_arm);
-        msg.data = "valid";
-    }
     pub_is_valid.publish(msg);
-    // pub_arm.publish(msg_out_arm);
+  } else {
+    SeedDistanceFunctor functor(seed);
+    std::priority_queue<std::vector<double>, std::vector<std::vector<double>>, SeedDistanceFunctor> slnQueue(
+        solutions.begin(), solutions.end(), functor);
+    kinematic_state->setJointGroupPositions(joint_model_group, slnQueue.top());
+  }
+
+  std::cerr << "Got " << solutions.size() << " solutions for " << joint_model_group->getName() << std::endl;
+
+  // Arm control
+
+  std::vector<double> joint_values;
+  kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+  return joint_values;
 }
 
-void RobotArm::publishGripperCommand(double gripper_pos)
-{
-    // Gripper control
-    using namespace victor_hardware_interface_msgs;
-    Robotiq3FingerCommand msg_out_gripper;
+void RobotArm::publishArmCommand(std::vector<double> joint_positions) {
+  victor_hardware_interface_msgs::MotionCommand msg_out_arm;
+  msg_out_arm.control_mode.mode = 2;
 
-    Robotiq3FingerActuatorCommand scissor;
-    scissor.speed = 1.0;
-    scissor.force = 1.0;
-    scissor.position = 1;
+  msg_out_arm.joint_position = victor_utils::vectorToJvq(joint_positions);
+  // Publish state messages
+  std_msgs::String msg;
+  msg.data = "invalid";
 
-    Robotiq3FingerActuatorCommand finger_a;
-    finger_a.speed = 1.0;
-    finger_a.force = 1.0;
-    finger_a.position = gripper_pos;
-
-    Robotiq3FingerActuatorCommand finger_b;
-    finger_b.speed = 1.0;
-    finger_b.force = 1.0;
-    finger_b.position = gripper_pos;
-
-    Robotiq3FingerActuatorCommand finger_c;
-    finger_c.speed = 1.0;
-    finger_c.force = 1.0;
-    finger_c.position = gripper_pos;
-
-    msg_out_gripper.scissor_command = scissor;
-    msg_out_gripper.finger_a_command = finger_a;
-    msg_out_gripper.finger_b_command = finger_b;
-    msg_out_gripper.finger_c_command = finger_c;
-
-    pub_gripper.publish(msg_out_gripper);
-
+  if (armWithinDelta(victor_utils::jvqToVector(msg_out_arm.joint_position), DELTA)) {
+    pub_arm.publish(msg_out_arm);
+    msg.data = "valid";
+  }
+  pub_is_valid.publish(msg);
+  // pub_arm.publish(msg_out_arm);
 }
 
-bool RobotArm::armWithinDelta(std::vector<double> joint_position_commanded, double delta)
-{
-    assert(joint_position_commanded.size() == joint_position_measured.size());
+void RobotArm::publishGripperCommand(double gripper_pos) {
+  // Gripper control
+  using namespace victor_hardware_interface_msgs;
+  Robotiq3FingerCommand msg_out_gripper;
 
-    double distance = 0;
+  Robotiq3FingerActuatorCommand scissor;
+  scissor.speed = 1.0;
+  scissor.force = 1.0;
+  scissor.position = 1;
 
-    for (int i = 0; i < joint_position_commanded.size(); ++i) {
-        distance += pow(joint_position_commanded[i] - joint_position_measured[i], 2);
-    }
+  Robotiq3FingerActuatorCommand finger_a;
+  finger_a.speed = 1.0;
+  finger_a.force = 1.0;
+  finger_a.position = gripper_pos;
 
-    distance = sqrt(distance);
+  Robotiq3FingerActuatorCommand finger_b;
+  finger_b.speed = 1.0;
+  finger_b.force = 1.0;
+  finger_b.position = gripper_pos;
 
-    std::cout << "Joint space error for " << joint_model_group->getName() << ": " << distance << std::endl;
+  Robotiq3FingerActuatorCommand finger_c;
+  finger_c.speed = 1.0;
+  finger_c.force = 1.0;
+  finger_c.position = gripper_pos;
 
-    return distance < delta;
+  msg_out_gripper.scissor_command = scissor;
+  msg_out_gripper.finger_a_command = finger_a;
+  msg_out_gripper.finger_b_command = finger_b;
+  msg_out_gripper.finger_c_command = finger_c;
+
+  pub_gripper.publish(msg_out_gripper);
+}
+
+bool RobotArm::armWithinDelta(std::vector<double> joint_position_commanded, double delta) {
+  assert(joint_position_commanded.size() == joint_position_measured.size());
+
+  double distance = 0;
+
+  for (int i = 0; i < joint_position_commanded.size(); ++i) {
+    distance += pow(joint_position_commanded[i] - joint_position_measured[i], 2);
+  }
+
+  distance = sqrt(distance);
+
+  std::cout << "Joint space error for " << joint_model_group->getName() << ": " << distance << std::endl;
+
+  return distance < delta;
 }
 
 void RobotArm::callbackArmStatusUpdate(victor_hardware_interface_msgs::MotionStatus msg) {
-    joint_position_measured = victor_utils::jvqToVector(msg.measured_joint_position);
-    // kinematic_state->setJointGroupPositions(joint_model_group, joint_position_measured);
-    
+  joint_position_measured = victor_utils::jvqToVector(msg.measured_joint_position);
+  // kinematic_state->setJointGroupPositions(joint_model_group, joint_position_measured);
 }
